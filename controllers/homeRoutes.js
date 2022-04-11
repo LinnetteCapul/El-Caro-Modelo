06b4b962ebfcda98b20fec21ab65a6f8a1b08e68
Yang Cheng <annacheng09@hotmail.com>
2022-04-11 14:00:51 -0700

    login will replace to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get('/cars/:id', async (req, res) => {
 router.get('/login', (req, res) => {
     // If the user is already logged in, redirect the request to another route
     if (req.session.logged_in) {
-        res.redirect('/profile');
+        res.replace('/');
         return;
     }
 
