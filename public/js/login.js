addca21d31dd4698b8a058fbf5341d3e6d6619c1
Yang Cheng <annacheng09@hotmail.com>
2022-04-12 10:51:39 -0700

    tested login on browser

--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,8 @@ const loginFormHandler = async (event) => {
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
+    console.log(email, password)
+
     if (email && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
