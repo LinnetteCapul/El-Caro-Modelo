5fd865d6745b9a2d3f4a7205975d1e7052f9264c
Yang Cheng <annacheng09@hotmail.com>
2022-04-12 11:20:17 -0700

    removed some of the consoles used in testing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -18,9 +18,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    console.log("I made it")
     const userData = await User.findOne({ where: { email: req.body.email } });
-    console.log(userData)
     if (!userData) {
       res
         .status(400)
@@ -29,7 +27,6 @@ router.post('/login', async (req, res) => {
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
-    console.log(validPassword)
     if (!validPassword) {
       res
         .status(400)
